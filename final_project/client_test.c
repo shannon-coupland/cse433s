0d7004de8e3ec0fa8cb7dcfd407597909c65cd47
Jason Oberstein <joberstein@wustl.edu>
2020-12-19 18:40:25 -0600

    initial timing tests

--- final_project/client_test.c
+++ final_project/client_test.c
@@ -1,73 +1,92 @@
-// Client side C/C++ program to demonstrate Socket programming 
+// Client side C/C++ program to demonstrate Socket programming
 // Reference: https://www.geeksforgeeks.org/socket-programming-cc/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h> 
+#include <sys/socket.h>
 #include <sys/un.h>
 #include <netinet/ip.h>
-#include <arpa/inet.h> 
-#include <unistd.h> 
-#include <string.h> 
-#define PORT 39393
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
 
-#define IP "128.252.167.161"
-#define IP2 "127.0.0.1"
+#define BUF_SIZE 256
+#define PORT 39393
+#define jason_ip "192.168.0.190"
+#define shan_ip "192.168.0.161"
+
+struct timespec req_before, req_after;
 
 const int max_expected_args = 2;
-   
-int main(int argc, char const *argv[]) 
-{
-    if (argc > max_expected_args) {
-	printf("Usage: ./client <quit (optional)>");
-	exit(-1);
-    }
 
-    int sock = 0; 
-    struct sockaddr_in serv_addr; 
+double test_creds(char* user, char* pass, int username_size, int password_size, int socket) {
+  send(socket, user, username_size, 0);
+  clock_gettime(CLOCK_MONOTONIC, &req_before);
+  send(socket, pass, password_size, 0);
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
-    { 
-        perror("Error creating socket");
-	exit(EXIT_FAILURE);
-    } 
-   
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-       
-    if(inet_pton(AF_INET, IP, &serv_addr.sin_addr) <= 0) {
-	perror("Error in init_pton");
-	exit(EXIT_FAILURE);
-    }
-    
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
-    { 
-        perror("Failed to connect");
-	exit(EXIT_FAILURE);
-    }
+  char response[BUF_SIZE];
+  if (read(socket, response, sizeof(response)) == -1) {
+    perror("read failed");
+    return -1;
+  }
 
-    //Send integer(s)
-    int n = 17;
-    int o = 65;
-    int p = 24;
+  clock_gettime(CLOCK_MONOTONIC, &req_after);
 
-    send(sock, &n, sizeof(n), 0);
-    send(sock, &o, sizeof(o), 0);
-    send(sock, &p, sizeof(p), 0);
+  return (req_after.tv_nsec - req_before.tv_nsec);
+}
 
-    printf("sent: %d\n", n);
-    printf("sent: %d\n", o);
-    printf("sent: %d\n", p);
+int main(int argc, char const *argv[]) {
+  if (argc > max_expected_args || argc < 1) {
+    printf("Usage: ./client <quit (optional)>");
+    exit(EXIT_FAILURE);
+  }
 
-    //If "quit" argument supplied, then send 418 to server to cause it to exit
-    if (argc == 2) {
-	if (strcmp(argv[1], "quit") == 0) {
-	    int q = 418;
-	    send(sock, &q, sizeof(q), 0);
-	    printf("sent: %d (quit)\n", q);
-	}
-    }
+  int sock = 0;
+  struct sockaddr_in serv_addr;
 
-    close(sock);
-    return 0; 
-} 
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("Error creating socket");
+    exit(EXIT_FAILURE);
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  if(inet_pton(AF_INET, jason_ip, &serv_addr.sin_addr) <= 0) {
+	  perror("Error in init_pton");
+	  exit(EXIT_FAILURE);
+  }
+
+  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("Failed to connect");
+	  exit(EXIT_FAILURE);
+  }
+
+  printf("connected\n");
+
+
+  char* username = "shan";
+  char* password = "shannon_password_super_secret";
+
+  //test1 (correct password)
+  double test1 = test_creds(username, password, sizeof(username), sizeof(password), sock);
+  if (test1 == -1) exit(EXIT_FAILURE);
+  printf("test1 (correct password) time (ns): %lf\n", test1);
+
+  //test2 (incorrect password, index 0 of 28)
+  password = "wrong_password";
+  double test2 = test_creds(username, password, sizeof(username), sizeof(password), sock);
+  if (test2 == -1) exit(EXIT_FAILURE);
+  printf("test2 (incorrect password, index 0) time (ns): %lf\n", test2);
+
+  //test3 (incorrect password, index 22 of 28)
+  password = "shannon_password_super_wrong_password";
+  double test3 = test_creds(username, password, sizeof(username), sizeof(password), sock);
+  if (test3 == -1) exit(EXIT_FAILURE);
+  printf("test3 (incorrect password, index 22 of 28) time (ns): %lf\n", test3);
+
+  close(sock);
+  return 0;
+
+}
--- final_project/server.c
+++ final_project/server.c
@@ -1,76 +1,101 @@
 // Server side C/C++ program to demonstrate Socket programming
 // Reference: https://www.geeksforgeeks.org/socket-programming-cc/
 
-#include <unistd.h> 
+#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h> 
-#include <stdlib.h> 
+#include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h> //used to be in.h from ex code, ip.h?
 #include <arpa/inet.h>
 #include <sys/un.h>
 #include <string.h>
+
+#define BUF_SIZE 256
 #define PORT 39393
 
-int main(int argc, char const *argv[]) 
-{ 
-    //int opt = 1;
-    int server_fd, new_socket;
-    struct sockaddr_in address; 
-    int addrlen = sizeof(address); 
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("Socket creation failed"); 
-        exit(EXIT_FAILURE); 
+#define SUCCESS "Successfully logged in!"
+#define NO_USER "Username does not exist."
+#define BAD_PASS "Wrong password - try again."
+
+#define CREDS_SIZE 2
+const char* CREDS[CREDS_SIZE][2] = {{"shan", "shannon_password_super_secret"},
+                                    {"jason", "jason_password_super_secret"}};
+
+
+int get_input(char* buf, int buf_size, int socket) {
+  printf("entered check_creds");
+  int result;
+
+  if ((result = read(socket, buf, buf_size)) < 0) {
+    perror("read failed");
+    return -1;
+  } else if (result == 0) {
+    perror("client closed");
+    return -1;
+  }
+
+  buf[buf_size - 1] = '\0';
+  return 0;
+}
+
+int check_creds(char* username, char* password, int socket) {
+  printf("entered check_creds");
+  for (int i = 0; i < CREDS_SIZE; i++) {
+    if (!strcmp(CREDS[i][0], username)) {
+      if (!strcmp(CREDS[i][1], password)) {
+        send(socket, SUCCESS, sizeof(SUCCESS), 0);
+      } else {
+        send(socket, BAD_PASS, sizeof(BAD_PASS), 0);
+      }
+      return 0;
     }
+  }
+  send(socket, NO_USER, sizeof(NO_USER), 0);
+  return 0;
+}
 
-    //if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        //perror("setsockopt failed");
-        //exit(EXIT_FAILURE);
-    //} 
-       
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-    
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    }
+int main(int argc, char const* argv[]) {
+  char* current_user;
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  int addrlen = sizeof(address);
 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    }
+  // Creating socket file descriptor
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-    int ex = 0;
-    while(!ex) {
-    	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-     	                  (socklen_t*)&addrlen))<0) 
-    	{ 
-     	   perror("accept"); 
-     	   exit(EXIT_FAILURE);
-    	} else {
-	   printf("New connection to socket\n");
-	}
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
 
-    	//Read integer(s)
-    	//Note: I attempted to use fscanf() per the studio assignment, but couldn't make
-    	//the client side work. I used recv() and send() instead.
-    	int c = 0;
-    	while(recv(new_socket, &c, sizeof(c), 0) > 0) {
-		printf("received: %d\n", c);
-		if (c == 418) {
-		    printf("Server closing\n");
-		    ex = 1;
-		}
-    	}
-    }
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
 
-    //unlink(MY_SOCK_PATH);
-    return 0; 
-} 
+  if (listen(server_fd, 3) < 0) {
+    perror("listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    perror("accept failed");
+  	exit(EXIT_FAILURE);
+  } else {
+    printf("New connection to socket\n");
+  }
+
+  while(1) {
+    char username[BUF_SIZE];
+    char password[BUF_SIZE];
+
+    if (get_input(username, sizeof(username), new_socket) == -1
+     || get_input(password, sizeof(password), new_socket) == -1) exit(EXIT_FAILURE);
+
+    check_creds(username, password, new_socket);
+  }
+
+  return 0;
+}
