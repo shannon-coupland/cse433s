b1a3c4f717ddbd5a2016150100d733167306ec6f
Jason Oberstein <joberstein@wustl.edu>
2020-12-20 13:14:53 -0600

    average implemented

--- final_project/server.c
+++ final_project/server.c
@@ -24,7 +24,7 @@ const char* CREDS[CREDS_SIZE][2] = {{"shan", "shannon_password_super_secret"},
 
 
 int get_input(char* buf, int buf_size, int socket) {
-    printf("entered get_input, about to read\n");
+    //printf("entered get_input, about to read\n");
     int result;
 
     if ((result = read(socket, buf, buf_size)) < 0) {
@@ -36,13 +36,13 @@ int get_input(char* buf, int buf_size, int socket) {
     }
 
     buf[buf_size - 1] = '\0';
-    printf("about to exit get_input\n");
+    //printf("about to exit get_input\n");
 
     return 0;
 }
 
 int check_creds(char* username, char* password, int socket) {
-    printf("entered check_creds\n");
+    //printf("entered check_creds\n");
     for (int i = 0; i < CREDS_SIZE; i++) {
         if (!strcmp(CREDS[i][0], username)) {
             if (!strcmp(CREDS[i][1], password)) {
@@ -54,7 +54,7 @@ int check_creds(char* username, char* password, int socket) {
         }
     }
     send(socket, NO_USER, sizeof(NO_USER), 0);
-    printf("about to exit check_creds\n");
+    //printf("about to exit check_creds\n");
     return 0;
 }
 
@@ -92,17 +92,17 @@ int main(int argc, char const* argv[]) {
      }
 
     while(1) {
-        printf("entered while loop\n");
+        //printf("entered while loop\n");
         char username[BUF_SIZE];
         char password[BUF_SIZE];
 
-        
+
         if (get_input(username, sizeof(username), new_socket) == -1) {
             printf("get_input returned -1 - exiting program\n");
             exit (EXIT_FAILURE);
         }
 
-        send (new_socket, GOT_USER, sizeof(got_user), 0);
+        send (new_socket, GOT_USER, sizeof(GOT_USER), 0);
 
         if (get_input(password, sizeof(password), new_socket) == -1) {
             printf("get_input returned -1 - exiting program\n");
